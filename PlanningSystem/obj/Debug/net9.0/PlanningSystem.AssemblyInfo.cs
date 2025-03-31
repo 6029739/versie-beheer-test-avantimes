184012cad318820d5a1358f53e10121b6cb59707
MrRosier <6029739@mborijnland.nl>
2025-03-31 09:42:12 +0200

    tijd lijn + dage

--- PlanningSystem/obj/Debug/net9.0/PlanningSystem.AssemblyInfo.cs
+++ PlanningSystem/obj/Debug/net9.0/PlanningSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlanningSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22db4e6b032f3aebaacd761b4651893f7ffaeb98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf5e3ad1bb6cebfd2d703d159309c5e80ded4d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlanningSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlanningSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
